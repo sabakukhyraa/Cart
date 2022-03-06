ec3c1785fca43324a8657fbbf0371f8d98e6b03d
Ali Kerem Ata <96057812+sabakukhyraa@users.noreply.github.com>
2022-03-06 14:24:32 +0300

    Vanilla JavaScript Practice/ Dynamization

---
+++ index.js
@@ -0,0 +1,14 @@
+const products = [
+    {id: 1, productName: "Apple - Iphone 13", unitPrice: 8900.0, img_src: "../Cart/assets/1.jpg"},
+    {id: 2, productName: "Gelliebro - Guitar", unitPrice: 2450.0, img_src: "../Cart/assets/2.jpg"},
+    {id: 3, productName: "Heinz - Conserve Beans", unitPrice: 14.75, img_src: "../Cart/assets/3.jpg"},
+    {id: 4, productName: "Converse - Shoes", unitPrice: 229.90, img_src: "../Cart/assets/4.jpg"},
+    {id: 5, productName: "Logitech - Headphone", unitPrice: 1350.0, img_src: "../Cart/assets/5.png"},
+    {id: 6, productName: "Mr.Pen - Tip Pen", unitPrice: 25.55, img_src: "../Cart/assets/6.jpg"},
+    {id: 7, productName: "Marvel's Avengers Comic Book", unitPrice: 51.90, img_src: "../Cart/assets/7.jpg"},
+    {id: 8, productName: "Selfie Stick", unitPrice: 37.75, img_src: "../Cart/assets/8.jpg"}
+]
+
+
+    
+
